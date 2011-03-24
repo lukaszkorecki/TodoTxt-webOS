5ca631fbaa6005b469881498ddbe40a770a6b826
Łukasz Korecki <lukasz@coffeesounds.com>
2011-03-24 23:21:25 +0000

    Taks model comments

--- app/models/tasks.js
+++ app/models/tasks.js
@@ -8,12 +8,18 @@ var Tasks = function(name, callbacks) {
 
 var _proto = Tasks.prototype;
 
+// TODO this needs to store a list of available keys for getAll query
+// and generate a key for example using:
+// atob(encodeURIComponent(content+done+date+priority)
 _proto.add = function(key, value, callbacks) {
   this.db.add(key, value, callbacks.onSuccess, callbacks.onFailure);
 };
+// TODO this needs to update key list (which is also stored in db)
 _proto.remove = function(key,  callbacks) {
   this.db.discard(key,  callbacks.onSuccess, callbacks.onFailure);
 };
+
+//
 _proto.get = function(key,  callbacks) {
   this.db.get(key,  callbacks.onSuccess, callbacks.onFailure);
 };
