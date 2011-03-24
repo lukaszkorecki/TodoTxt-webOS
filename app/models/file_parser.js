f94b773aeb72c2a5865412a10186ec60dde33346
Łukasz Korecki <lukasz@coffeesounds.com>
2011-03-24 08:32:00 +0000

    Line parsing tests (1 failing!)

--- app/models/file_parser.js
+++ app/models/file_parser.js
@@ -1,2 +1,29 @@
-var FileParser = function() {
+var FileParser = function(lineSeparator) {
+  this.lineSeparator = lineSeparator || "\n";
+
+  this.priorityRegex = /^\(\w\)/i;
+  this.doneRegex = /^x\ /i;
+  this.dateRegex = /\d{4}-\d{2}-\d{2}/i;
+};
+
+FileParser.prototype.load = function(fileContent) {
+  this.content = fileContent.split(this.lineSeparator);
+};
+
+FileParser.prototype.parseLine = function(line) {
+
+  var priority = line.match(this.priorityRegex)[0];
+  var isDone = !!(line.match(this.doneRegex));
+  var createdAt = line.match(this.dateRegex)[0];
+
+  var content = line.replace(this.priorityRegex, '');
+  content.replace(this.doneRegex,'');
+  content.replace(this.dateRegex,'');
+
+  var ret = { content : content.trim(), done : isDone };
+
+  if(createdAt) ret['createdAt'] = createdAt;
+  if(priority) ret['priority'] = priority[1];
+  console.dir(ret);
+  return ret;
 };
